95961a5a2dbfd8bf89ffa0b64fda4ed35352bc5e
Tom Beckmann <tomjonabc@gmail.com>
2018-04-25 09:11:10 +0200

    implement saving of page content

--- src/app/page-content/page-content.component.ts
+++ src/app/page-content/page-content.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Page } from '../page';
+import { ElementRef, ViewChild, OnChanges, Component, OnInit, Input } from '@angular/core'
+import { Subject } from 'rxjs/Subject'
+import { Page } from '../page'
+import { NoteService } from '../note.service'
+
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/mergeMap'
+import 'rxjs/add/operator/do'
+
+const SAVING_TIMEOUT = 2000
 
 @Component({
   selector: 'app-page-content',
   templateUrl: './page-content.component.html',
   styleUrls: ['./page-content.component.css']
 })
-export class PageContentComponent implements OnInit {
+export class PageContentComponent implements OnInit, OnChanges {
   @Input () page: Page;
+  @ViewChild('content') contentBox: ElementRef;
 
-  constructor() { }
+  saved = true
+  contentChanged = new Subject<string>()
+
+  constructor(private server: NoteService) { }
 
   ngOnInit() {
+    this.contentChanged
+      .do(() => this.saved = false)
+      .debounceTime(SAVING_TIMEOUT)
+      .distinctUntilChanged()
+      .mergeMap(save => this.server.updatePage(this.page))
+      .subscribe(() => this.saved = true)
   }
 
+  changeContent() {
+    // uses the value saved within the object
+    this.contentChanged.next('')
+  }
+
+  ngOnChanges() {
+    if (!this.contentBox)
+      return
+    this.contentBox.nativeElement.style.height = 0
+    this.contentBox.nativeElement.clientHeight
+    this.contentBox.nativeElement.style.height = this.contentBox.nativeElement.scrollHeight + 2 + 'px'
+  }
 }
